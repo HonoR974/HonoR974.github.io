fa0a3ca67172a2b9733923d6c28f603f62ff3a2e
Honor <honore.guillaudeau1@gmail.com>
2022-10-03 13:35:08 +0200

    Derniere MAJ Faites
    A mettre en ligne

--- src/script.js
+++ src/script.js
@@ -43,7 +43,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 3000); // Change image every 5 seconds
 }
 
 
@@ -55,19 +55,29 @@ function showSlides() {
 filterSelection("all")
 
 function filterSelection(c) {
+
   var x, i;
   x = document.getElementsByClassName("filterDiv");
+
+
   if (c == "all") c = "";
 
   for (i = 0; i < x.length; i++) {
 
     w3RemoveClass(x[i], "show");
+
+
+
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
     
   }
+
+
 }
 
+// Add 
 function w3AddClass(element, name) {
+
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -76,26 +86,50 @@ function w3AddClass(element, name) {
   }
 }
 
+// remove 
 function w3RemoveClass(element, name) {
+
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
+
+
   for (i = 0; i < arr2.length; i++) {
+
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);     
     }
   }
   element.className = arr1.join(" ");
-}
 
+}
+ 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
+
 var btns = btnContainer.getElementsByClassName("btn");
+
 for (var i = 0; i < btns.length; i++) {
+
   btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+
+    console.log("this " + this.className +  " id " +this.id);
+    
+
+    var current = document.getElementsByClassName(" active");
+
+
+
+    console.log( " nmb d'actif " + current.length);
+    console.log("info " + current[0].id);
+
+  current[0].className = current[0].className.replace(" active", " ");
+    //current[i].className = current[i].className.replace("", " active");
+   this.className += " active";
+  
+   console.log("this " + this.className +  " id " +this.id );
+
+
   });
 }
 
@@ -109,7 +143,6 @@ var btn ;
 // When the user clicks the button, open the modal 
 function openModal(id)
 {
-  console.log(" openModal " + id);
   // Get the button that opens the modal
   btn = document.getElementById(id);
   // Get the modal
@@ -122,7 +155,6 @@ function openModal(id)
 // When the user clicks on <span> (x), close the modal
 function closeModal(id)
 {
-  console.log(" closeModal " + id);
 // Get the <span> element that closes the modal
 var span = document.getElementById(id);
 
@@ -144,22 +176,23 @@ window.onclick = function(event) {
 
 function read(id) {
 
-  console.log("++++++++++++++debut read ");
+
 
   var dots = document.getElementById("dots" + id);
   var moreText = document.getElementById("more"+id);
   var btnText = document.getElementById( id);
 
-  console.log( " id dots " +  id) + "\n dots " + dots;
+
 
   if (dots.style.display.length < 1 )
   {
     dots.style.display = "none";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
   }
 
 
 
-  console.log(dots.style.display);
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -171,5 +204,4 @@ function read(id) {
     moreText.style.display = "inline";
   }
 
-  console.log(dots.style.display);
 }
